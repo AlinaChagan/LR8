e4fe2997256a78074842eff12aaf955b16f52295
Alina Chagan <90026846+AlinaChagan@users.noreply.github.com>
2022-01-16 17:39:54 +0300

    LR_8

---
+++ LR8/IGeneric.cs
@@ -0,0 +1,11 @@
+﻿namespace Laba8
+{
+    public interface IGeneric<T>
+    {
+        /*1.Создайте обобщенный интерфейс  операциями  добавить, удалить,просмотреть.*/
+        void Add(T item);
+        void Delete(T item);
+        void Show();
+    }
+
+}
